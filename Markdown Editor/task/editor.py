dba5caf4526961f7c2661e07135241a6babd16e8
drtierney <d.r.tierney@hotmail.co.uk>
2021-03-28 18:40:29 +0100

    Stage #2 completed
    Stage 2/5: How do I use it?

--- Markdown Editor/task/editor.py
+++ Markdown Editor/task/editor.py
@@ -1,10 +1,24 @@
-print('''
-# John Lennon
-or ***John Winston Ono Lennon*** was one of *The Beatles*.
-Here are the songs he wrote I like the most:
-* Imagine
-* Norwegian Wood
-* Come Together
-* In My Life
-* ~~Hey Jude~~ (that was *McCartney*)
-''')
+formats = ["plain", "bold", "italic",
+           "header", "link", "inline-code",
+           "ordered-list", "unordered-list", "new-line"]
+
+commands = ["!help", "!done"]
+
+
+def print_help():
+    print("Available formatters: {0}".format(" ".join(formats)))
+    print("Special commands: {0}".format(" ".join(commands)))
+
+
+done = False
+
+while not done:
+    user_input = input("- Choose a formatter: ")
+    if user_input == "!done":
+        done = True
+    elif user_input == "!help":
+        print_help()
+    elif user_input in formats:
+        pass
+    else:
+        print("Unknown formatting type or command. Please try again")
